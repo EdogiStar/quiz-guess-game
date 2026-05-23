6c21effaa59ddfd73f60f67ed3e225cb1f51040d
Isah Muhammad <alhajimuhammadisah@gmail.com>
2026-05-23 22:43:14 +0100

    fixed header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <>
       <header className="bg-white shadow-md">
-        <div className="max-w-5xl mx-auto px-4 py-4 flex-items-center justify-between">
+        <div className="max-w-5xl mx-auto px-4 py-4 flex items-center justify-between">
           <h1 className="text-2xl font-bold text-blue-600">
           Quiz Game
           </h1>
