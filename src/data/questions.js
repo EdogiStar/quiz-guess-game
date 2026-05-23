da879771d06f29297bdcfe8a3e1ed075d6304653
Isah Muhammad <alhajimuhammadisah@gmail.com>
2026-05-23 23:22:06 +0100

    added question objects

---
+++ src/data/questions.js
@@ -0,0 +1,23 @@
+
+const questions = [
+  {
+    id: 1,
+    question: "What is the capital of Nigeria?",
+    options: ["Lagos", "Abuja", "Kano", "Port Harcourt"],
+    answer: "Abuja"
+  },
+  {
+    id: 2,
+    question: "Which language is used for web styling?",
+    options: ["HTML", "Python", "CSS", "C++"],
+    answer: "CSS"
+  },
+  {
+    id: 3,
+    question: "What does JS stand for in web development?",
+    options: ["Java System", "JavaScript", "Just Script", "JSON Style"],
+    answer: "JavaScript"
+  }
+];
+
+export default questions;
