cc06950b0e9fe79c7b3ac56bc0cb67298a454349
Isah Muhammad <alhajimuhammadisah@gmail.com>
2026-05-24 17:00:09 +0100

    useState added

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -1,7 +1,11 @@
+import { useState } from 'react'
 import questions from '../data/questions'
 
 function QuestionCard(){
-    const currentQuestion = questions[0];
+    // State for current question index
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    //current question 
+    const currentQuestion = questions[currentQuestionIndex];
     
     // Handle answer click
     const handleAnswerClick = (selectedOption) => {
@@ -9,17 +13,24 @@ function QuestionCard(){
 
     // Temporary testing
     alert(`You selected: ${selectedOption}`);
-  };
+    
+    // Handle next question 
+    const handleNextQuestion = () => 
+        {
+            setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+        }
+    };
     return (
         <>
           <div className="max-w-md w-full bg-white rounded-2xl shadow-lg p-6 space-y-6">
             
             <div>
+            { /* Questions  */}
               <h2 className="text-2xl font-semibold text-gray-800 leading-snug">
                 {currentQuestion.question}
               </h2>
             </div>
-            
+            { /* Options */}
             <div className="flex flex-col gap-4">
               {
                   currentQuestion.options.map((option, index ) => (
@@ -29,8 +40,8 @@ function QuestionCard(){
                   ))
               }
             </div>
-            
-            <button className="w-full bg-blue-600 text-white py-3 rounded-lg font-medium hover:bg-blue-700 transition duration-200">
+            { /*  Next Question  */}
+            <button onClick={handleNextQuestion} className="w-full bg-blue-600 text-white py-3 rounded-lg font-medium hover:bg-blue-700 transition duration-200">
               Next Question
             </button>
           </div>
